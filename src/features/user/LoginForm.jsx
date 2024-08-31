a16a400ae3156fecd39e2f12f16e7bc55b44ca5b
SwamYi <swamyi@gmail.com>
2024-09-01 01:29:31 +0630

    login logic

--- src/features/user/LoginForm.jsx
+++ src/features/user/LoginForm.jsx
@@ -1,29 +1,84 @@
-import React from 'react'
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button } from "react-bootstrap";
+import { getLoginStatus, login } from "../auth/authSlice";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = (props) => {
-    return (
-        <Form className={props.className} onAnimationEnd={props.onAnimationEnd}> 
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-    
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-          </Form.Group>
-          <Button style={{width: "100%"}} variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-    )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const loginStatus = useSelector(getLoginStatus);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onEmailInputChange = (e) => {
+    setEmail(e.target.value);
+  };
+  const onPasswordInputChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onLogin = (email, password) => {
+    dispatch(
+      login({
+        username: email,
+        password: password,
+      })
+    );
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    onLogin(email, password);
+  };
+
+  useEffect(() => {
+    if (loginStatus === "success") {
+      navigate("/");
+    }
+  }, [loginStatus, navigate]);
+
+  return (
+    <Form
+      className={props.className}
+      onAnimationEnd={props.onAnimationEnd}
+      onSubmit={onSubmit}
+    >
+      {loginStatus === "failed" && (
+        <Form.Text className="text-danger">
+          Login failed. Incorrect username or password
+        </Form.Text>
+      )}
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          onChange={onEmailInputChange}
+          type="text"
+          placeholder="Enter email"
+          value={email}
+        />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          onChange={onPasswordInputChange}
+          type="password"
+          placeholder="Password"
+          value={password}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button style={{ width: "100%" }} variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 
 export default LoginForm;
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import regionSlice from "../features/region/regionSlice";
 import menuSlice from "../features/foods/foodSlice";
 import cartSlice from "../features/cart/cartSlice";
 import restaurantSlice from "../features/restaurant/restaurantSlice";
+import authSlice from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
@@ -10,6 +11,6 @@ export const store = configureStore({
     menus: menuSlice,
     cart: cartSlice,
     restaurants: restaurantSlice,
-    
+    auth: authSlice,
   },
 });
